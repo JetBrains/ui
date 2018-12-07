59558bfaec5e496347824f589212c59c738bae65
alina.mishina <alina.mishina@jetbrains.com>
2018-12-07 20:28:30 +0300

    Add anchors.js script

---
+++ scripts/anchors.js
@@ -0,0 +1,35 @@
+function setAnchors() {
+    var index = 0
+    $('article > p').each(function() {
+        var text = ''
+        index++
+        text = index < 10 ? '0' + index : index
+
+        var block = '<div class="block">' +
+            '<a class="anchor" href="#' + text + '" name="' + text + '">' + text + '</a>' +
+            '<p>' + $(this).html() + '</p>' +
+            '</div>'
+
+        $(this).replaceWith(block)
+    })
+    $('article > h2').each(function() {
+        $(this).addClass('anchor')
+        $(this).attr('name', $(this).attr('id'))
+    })
+
+    $('article .anchor').click(function(event) {
+        var $anchor = $(this)
+        var name = $anchor.attr('name')
+
+        $('html, body').animate({
+            scrollTop: $anchor.offset().top - 20
+        }, 300, function() {
+            if (history.pushState) {
+                history.pushState(null, '', '#' + name)
+            } else {
+                location.hash = name
+            }
+        })
+        event.preventDefault()
+    })
+}
