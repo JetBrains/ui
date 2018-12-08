cef5448dc65755ccf85a75d97f4eb0ecbf10598e
alinamishina <mishinalina@gmail.com>
2018-12-08 21:49:42 +0300

    Do not add anchors for <p class="anchor> elements. Do not add anchors for paragraphs that contain only image

--- scripts/anchors.js
+++ scripts/anchors.js
@@ -1,25 +1,31 @@
 function setAnchors() {
-    var index = 0
-    $('article > p').each(function() {
-        var text = ''
-        index++
-        text = index < 10 ? '0' + index : index
+    function hasSingleImageInside(p) {
+        var img = p.find('img');
+        return img.length > 0 && p.children().length === 1 && p.text().trim().length === 0
+    }
+
+    var index = 0;
+    $('article > p').each(function () {
+        if (hasSingleImageInside($(this)) || $(this).hasClass('noanchor')) {
+            return;
+        }
+        var text = '';
+        index++;
+        text = index < 10 ? '0' + index : index;
 
         var block = '<div class="block">' +
-            '<a class="anchor" href="#' + text + '" name="' + text + '">' + text + '</a>' +
-            '<p>' + $(this).html() + '</p>' +
-            '</div>'
-
+            '<a class="paragraphAnchor" href="#' + text + '" name="' + text + '">' + text + '</a>' + $(this).prop('outerHTML')
+            + '</div>';
         $(this).replaceWith(block)
-    })
-    $('article > h2').each(function() {
-        $(this).addClass('anchor')
+    });
+    $('article > h2, h3, h4, h5, h6').each(function () {
+        $(this).addClass('headerAnchor');
         $(this).attr('name', $(this).attr('id'))
-    })
+    });
 
-    $('article .anchor').click(function(event) {
-        var $anchor = $(this)
-        var name = $anchor.attr('name')
+    $('.paragraphAnchor, .headerAnchor').click(function (event) {
+        var $anchor = $(this);
+        var name = $anchor.attr('name');
 
         this.scrollIntoView({
             block: 'start',
