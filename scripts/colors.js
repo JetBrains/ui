1bad52be96fa4a7f0a244ec063c8b02b1e63d4df
alinamishina <mishinalina@gmail.com>
2019-02-18 00:37:21 +0300

    Cleanup swatches
    - extract colors.js
    - take theme names from color.js

---
+++ scripts/colors.js
@@ -0,0 +1,62 @@
+const COLORS = {
+    "Tooltip.errorBackground": {
+        "Mac OS": "#F5E6E7",
+        "Windows": "#F5E6E7",
+        "IntelliJ": "#F5E6E7",
+        "Darcula": "#593D41",
+    },
+    "Tooltip.errorBorder": {
+        "Mac OS": "#E0A8A9",
+        "Windows": "#E0A8A9",
+        "IntelliJ": "#E0A8A9",
+        "Darcula": "#73454B"
+    },
+    "Input.errorFocusBorder": {
+        "Mac OS": "rgba(245, 59, 59, 0.5)",
+        "Windows": "#F57D7D",
+        "IntelliJ": "#E53E4D",
+        "Darcula": "#8B3C3C"
+    },
+    "Input.errorFocusBorderInactive": {
+        "Mac OS": "rgba(247, 188, 188, 0.5)",
+        "Windows": "#EECACA",
+        "IntelliJ": "#EBBCBC",
+        "Darcula": "#725252"
+    },
+    "Text.errorForeground": {
+        "Mac OS": "#FF0000",
+        "Windows": "#FF0000",
+        "IntelliJ": "#FF0000",
+        "Darcula": "#FF5261"
+    },
+    "Tooltip.warningBackground": {
+        "Mac OS": "#F5F0E6",
+        "Windows": "#F5F0E6",
+        "IntelliJ": "#F5F0E6",
+        "Darcula": "#594E32"
+    },
+    "Tooltip.warningBorder": {
+        "Mac OS": "#E0CEA8",
+        "Windows": "#E0CEA8",
+        "IntelliJ": "#E0CEA8",
+        "Darcula": "#78550B"
+    },
+    "Input.warningFocusBorder": {
+        "Mac OS": "rgba(238, 178, 62, 0.5)",
+        "Windows": "#E1A336",
+        "IntelliJ": "#E1A336",
+        "Darcula": "#AD7A0F"
+    },
+    "Input.warningFocusBorderInactive": {
+        "Mac OS": "rgba(255, 220, 146, 0.5)",
+        "Windows": "#EAD2A1",
+        "IntelliJ": "#EAD2A1",
+        "Darcula": "#6F531F"
+    },
+    "Text.warningForeground": {
+        "Mac OS": "#E77D00",
+        "Windows": "#E77D00",
+        "IntelliJ": "#E77D00",
+        "Darcula": "#DC9B2F"
+    }
+};
--- scripts/swatch.js
+++ scripts/swatch.js
@@ -1,132 +1,74 @@
 function createSwatchTables() {
-  $('.swatchTable').each(function (index) {
-    const table = this
-    const colorsData = table.dataset.colors
-    const colorKeys = colorsData.split(',')
+    $('.swatchTable').each(function () {
+        const table = this;
+        const colorsData = table.dataset.colors;
+        const colorKeys = colorsData.split(',');
 
-    const themes = ["Mac OS", "Windows", "IntelliJ", "Darcula"]
-    let output = `<div class="swatchTable">`
+        let themes = new Set();
+        colorKeys.forEach(colorKey => {
+            Object.keys(COLORS[colorKey]).forEach(theme => themes.add(theme));
+        });
+        let output = `<div class="swatchTable">`;
 
-    output += `<div class="table-line themeTabs">`
-    themes.map((theme, idx) => {
-      let classNames = "theme"
-      if (idx === 0) classNames += " active"
-      output += `<span class="${classNames}" data-idx="${idx}">${theme}</span>`
+        output += `<div class="table-line themeTabs">`;
+        let first = true;
+        themes.forEach(theme => {
+            let classNames = "theme";
+            if (first) {
+                classNames += " active";
+                first = false
+            }
+            output += `<span class="${classNames}">${theme}</span>`
+        });
+        output += "</div>";
+
+        output += `<table class="swatches">`;
+        output += "<colgroup>";
+        output += `<col style="width: 35%">`;
+        output += `<col>`;
+        output += "</colgroup>";
+        output += "<tbody>";
+
+        colorKeys.forEach(key => {
+            const values = COLORS[key];
+            if (!values) {
+                console.error(`Can't find color values for: '${key}'`);
+                return
+            }
+            const defaultColor = values["Mac OS"];
+            output += `<tr class="table-line">`;
+            output += `<td class="color-key">${key}</td>`;
+            output += `<td class="colorValue" data-color-key="${key}">`;
+            output += `<span class="block" style="background: ${defaultColor}"></span>`;
+            output += `<span class="hex-code">${defaultColor}</span>`;
+            output += "</td>";
+            output += "</tr>"
+        });
+
+        output += "</tbody>";
+        output += "</table>";
+
+        output += "</div>";
+
+        $(table).append($(output));
+        attachSwatchClickHandlers();
     })
-    output += "</div>"
-
-    output += `<table class="swatches">`
-    output += "<colgroup>"
-    output += `<col width="35%">`
-    output += `<col width="65%">`
-    output += "</colgroup>"
-    output += "<tbody>"
-
-    colorKeys.map(key => {
-      const values = COLORS[key]
-      if (!values) {
-        console.error(`Can't find color values for: '${key}'`)
-        return
-      }
-      const defaultColor = values[0]
-      output += `<tr class="table-line">`
-      output += `<td>${key}</td>`
-      output += `<td class="colorValue" data-values='${values.map(v => { return `${v}` }).join('|') }'>`
-      output += `<span class="block" style="background: ${defaultColor}"></span>`
-      output += `<span class="hexCode">${defaultColor}</span>`
-      output += "</td>"
-      output += "</tr>"
-    })
-
-    output += "</tbody>"
-    output += "</table>"
-
-    output += "</div>"
-
-    $(table).append($(output))
-    attachSwatchClickHandlers();
-  })
 }
 
-function attachSwatchClickHandlers(tab) {
-  $('.themeTabs .theme').click(function () {
-    const idx = this.dataset.idx
-    
-    // Change tabs
-    const tabs = $(this).parent()
-    tabs.find('.active').removeClass('active')
-    $(this).addClass('active')
+function attachSwatchClickHandlers() {
+    $('.themeTabs .theme').click(function () {
+        const theme = $(this).text();
+        // Change tabs
+        const tabs = $(this).parent();
+        tabs.find('.active').removeClass('active');
+        $(this).addClass('active');
 
-    // Change colors in table
-    const table  = $(this).parent().parent()
-    table.find('.colorValue').each(function() {
-      const values = this.dataset.values.split('|')
-      const hex = values[idx]
-      $(this).find('.block').css('background', hex)
-      $(this).find('.hexCode').text(hex)
+        // Change colors in table
+        const table = $(this).parent().parent();
+        table.find('.colorValue').each(function () {
+            const hex = COLORS[this.dataset.colorKey][theme];
+            $(this).find('.block').css('background', hex);
+            $(this).find('.hex-code').text(hex)
+        })
     })
-  })
-}
-
-const COLORS = {
-  "Tooltip.errorBackground" : [
-    "#F5E6E7", 
-    "#F5E6E7", 
-    "#F5E6E7",
-    "#593D41"
-  ],
-  "Tooltip.errorBorder": [
-    "#E0A8A9",
-    "#E0A8A9",
-    "#E0A8A9",
-    "#73454B"
-  ],
-  "Input.errorFocusBorder": [
-    "rgba(245, 59, 59, 0.5)",
-    "#F57D7D",
-    "#E53E4D",
-    "#8B3C3C"
-  ],
-  "Input.errorFocusBorderInactive": [
-    "rgba(247, 188, 188, 0.5)",
-    "#EECACA",
-    "#EBBCBC",
-    "#725252"
-  ],
-  "Text.errorForeground": [
-    "#FF0000",
-    "#FF0000",
-    "#FF0000",
-    "#FF5261"
-  ],
-  "Tooltip.warningBackground" : [
-    "#F5F0E6", 
-    "#F5F0E6", 
-    "#F5F0E6",
-    "#594E32"
-  ],
-  "Tooltip.warningBorder": [
-    "#E0CEA8",
-    "#E0CEA8",
-    "#E0CEA8",
-    "#78550B"
-  ],
-  "Input.warningFocusBorder": [
-    "rgba(238, 178, 62, 0.5)",
-    "#E1A336",
-    "#E1A336",
-    "#AD7A0F"
-  ],
-  "Input.warningFocusBorderInactive": [
-    "rgba(255, 220, 146, 0.5)",
-    "#EAD2A1",
-    "#EAD2A1",
-    "#6F531F"
-  ],
-  "Text.warningForeground": [
-    "#E77D00",
-    "#E77D00",
-    "#E77D00",
-    "#DC9B2F"
-  ]
 }
